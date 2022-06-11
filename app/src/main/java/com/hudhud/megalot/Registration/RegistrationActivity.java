a75a1eaca1a0a9e5819d017d77fcc630341cbba3
samar-elias <76810297+samar-elias@users.noreply.github.com>
2022-06-11 10:42:27 +0300

    first create

---
+++ app/src/main/java/com/hudhud/megalot/Registration/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.hudhud.megalot.Registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hudhud.megalot.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
